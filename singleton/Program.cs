6640d7cbdce30621bf907ac66ca0d8bfed627efe
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-02-17 17:43:35 +0300

    add singleton example

---
+++ singleton/Program.cs
@@ -0,0 +1,67 @@
+﻿
+new Application().Run();
+
+class Application
+{
+    public void Run()
+    {
+
+        Thread process1 = new Thread(() =>
+        {
+            TestSingleton("One");
+        });
+
+        Thread process2 = new Thread(() =>
+        {
+            TestSingleton("Two");
+        });
+
+        process1.Start();
+        process2.Start();
+
+        process1.Join();
+        process2.Join();
+
+    }
+
+    public static void TestSingleton(string value)
+    {
+        var instance = Singleton.GetInstance(value);
+        Console.WriteLine(instance.Value);
+    }
+}
+
+
+
+class Singleton
+{
+    private static Singleton _instance;
+
+    private static readonly object _lock = new object();
+
+    public string Value { get; set; }
+
+    private Singleton()
+    {
+
+    }
+
+    public static Singleton GetInstance(string value)
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                    _instance.Value = value;
+                }
+            }
+
+        }
+
+        return _instance;
+    }
+
+}
